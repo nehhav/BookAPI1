a619c3a272c8bde988fb35be3aaeaecf0f302d42
nehhav <nehhav@hotmail.com>
2021-10-16 16:52:50 +0530

    PUT AND DELETE

---
+++ operator.js
@@ -0,0 +1,22 @@
+//MongoDB Operators -> Powerfull
+
+//Logical Operators
+
+//$inc => increment ,+1,+2,+3, -1 ,-2,-3
+//$min => minimum
+//$max => maximum
+//$set => Used to set a data
+//book.title ="xyz"
+
+//$unset => removing a property from an object
+
+//Array operators
+//$push
+//name = ["neha", "C134"]
+//$pop
+//used to extract/remove/delete the last element
+//$pull
+//pull: {
+//name: "NEHA"
+//}
+//$addToSet => doesn't allow duplicates
